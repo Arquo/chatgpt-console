9e57309cdf9d1b9e655a13da37d4e737c8141ba1
r <chenray2003@gmail.com>
2024-04-20 19:26:08 -0400

    user input and button

---
+++ components/buttons.tsx
@@ -0,0 +1,34 @@
+"use client"
+import React, { useState } from 'react';
+
+interface ButtonProps {
+  onClick: (input: string) => void;
+}
+const InputButtonComponent: React.FC<ButtonProps> = ({ onClick }) => {
+    // onClick function is taken from the page 
+  const [userInput, setUserInput] = useState("");
+
+  const handleButtonClick = () => {
+    onClick(userInput);
+    // userInput -> setDisplayedInput(userInput)
+  };
+  return (
+    <div className="flex flex-col items-center space-y-4">
+      <input 
+        type="text" 
+        value={userInput} 
+        onChange={(e) => setUserInput(e.target.value)}  
+        className="w-full bg-white p-2 rounded-md"
+        placeholder="Type your question here..." 
+      />
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        onClick={handleButtonClick}>
+        {/* This is a real react button, when it is clicked, call handleButtonClick*/}
+        Submit
+      </button>
+    </div>
+  );
+}
+
+export default InputButtonComponent;
